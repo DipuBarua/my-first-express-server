52e8c5df74e7d04a3a3bfbc484c93494fcfcde66
DipuBarua <you@example.com>
2023-10-11 14:39:23 +0600

    my first express server created

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const express = require('express')
+const phones = require('./phones.json');
+const cors = require('cors');
+const app = express()
+const port = 5000
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Phone server on Live...')
+})
+
+app.get('/phones', (req, res) => {
+    res.send(phones)
+});
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('phone id:', id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone);
+})
+
+app.listen(port, () => {
+    console.log(`Phone server on port ${port}`)
+})
